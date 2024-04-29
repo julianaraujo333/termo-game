addd7e2dde8551b8b5fdf5ed0c965ee58f63a122
Juliana <julianaraujo555@outlook.com>
2024-04-29 11:33:36 -0300

    detecting keypress

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,59 @@
-console.log('HELLO');
+const lettersPattern = /[a-z]/;
+const currentGuessCount = 1;
+const currentGuess = document.querySelector('#guess' + currentGuessCount);
+const currentLetter = currentGuess.dataset.letters;
 
-const test = () => {
-	console.log('this is a test');
+document.addEventListener('keydown', (e) => {
+	let keypress = e.key;
+
+	console.log(e)
+  if (currentGuessCount < 7) {
+    if (
+      keypress.length == 1 &&
+      lettersPattern.test(e.key) 
+    ) {
+			updateLetters(keypress);
+      console.log('is letter');
+    } else if (e.key == 'Backspace' && currentGuess.dataset.letters != '' || e.key === "Delete" && currentGuess.dataset.letters != '') {
+			// const focusedTile = document.activeElement;
+			// console.log('focus' + focusedTile.id)
+      deleteFromLetters();
+    } else if (e.key == 'Enter' && currentGuess.dataset.letters.length == 5) {
+      // submitGuess();
+    }
+  }
+})
+
+/**
+ * Updates the entered letters for the current guess and updates corresponding tiles.
+ */
+
+const updateLetters = (letter) => {
+	let oldLetters = currentGuess.dataset.letters;
+	let newLetters = oldLetters + letter; 
+	let currentTile = newLetters.length;
+	currentGuess.dataset.letters = newLetters;
+
+	updateTiles(currentTile, letter);
+}
+
+const updateTiles = (tileNumber, letter) => {
+	let currentTile = document.querySelector('#guessTile' + tileNumber);
+	currentTile.innerText = letter;
+}
+
+
+/**
+ * Delete last letter
+ */
+const deleteFromLetters = () => {
+  let oldLetters = currentGuess.dataset.letters;
+  let newLetters = oldLetters.slice(0, -1);
+  currentGuess.dataset.letters = newLetters;
+	
+  deleteFromTiles(oldLetters.length);
 };
+
+const deleteFromTiles = (tileNumber) =>{
+	document.querySelector('#guessTile' + tileNumber).innerText = "";
+}
